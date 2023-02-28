2126fd7f764543a18a9e3ed1149aae642191b3ec
JoshAmpofo <joshampofo92@gmail.com>
2023-02-28 19:18:44 +0000

    Perform matching of VoIP

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# This script performs pattern matching over a text file
+# The search pattern should return:
+#   [SENDER],[RECEIVER],[FLAGS}
+# Sender: phone number or name (including country code if present)
+# Receiver: same details as sender
+# Flags that were used
+
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(,)
