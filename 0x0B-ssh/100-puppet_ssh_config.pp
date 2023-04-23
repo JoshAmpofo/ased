e036c16ded1444e3c57bd39fdb78011490422355
JoshAmpofo <joshampofo92@gmail.com>
2023-04-23 11:48:08 +0000

    Config ssh_config using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,17 @@
 # This manifest will makes changes to ssh_config file
+include stdlib
 
-file { '/etc/ssh/ssh_config':
-  content => '
-    Host *
-		HostName 54.197.44.5
-		User ubuntu
-		IdentityFile ~/.ssh/school
-		PasswordAuthentication no',
-    }
+
+file_line { 'Refuse password authentication':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+  }
+
+file_line { 'Set private key':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school'
+  replace => true,
+  }
